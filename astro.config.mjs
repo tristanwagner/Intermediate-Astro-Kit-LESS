6bd32310ff44a6b4a4a6336d3ea67aa54deda3ae
tristanwagner <t.wagner66250@gmail.com>
2024-01-22 18:03:31 +0900

    remove unused netlify identity & try prerender
    prerender & prefecth options were introduced in astro v4 and is not yet widely
    supported by browser but it can improve loading times

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,11 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  experimental: {
+    clientPrerender: true,
+  },
 });
