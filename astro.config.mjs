827654e0fbb324e26aba707152a2bc535d948d88
tristanwagner <t.wagner66250@gmail.com>
2024-02-13 16:39:24 +0900

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'viewport',
+    defaultStrategy: 'viewport'
   },
   experimental: {
-    clientPrerender: true,
+    clientPrerender: true
   },
+  integrations: [compress()]
 });
